928ba901fb9dbb4cff1155e038973b2da1a98c5a
MinyoungKim <801sanae@gmail.com>
2020-07-12 18:00:46 +0900

    Initial Commit

---
+++ vcTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vcTest
+//
+//  Created by 김민영 on 7/12/20.
+//  Copyright © 2020 MINYOUNGKIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
